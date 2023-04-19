c25b654f2e6dd27ca3b7a831e38a19250f71225e
nek0722 <220439@astanait.edu.kz>
2023-04-19 19:56:21 +0600

    start the implementation of tests

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,11 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        MyArrayList <String> arrList = new MyArrayList<>();
-        MyLinkedList <String> linkList = new MyLinkedList<>();
+        MyArrayList <String> arrListStr = new MyArrayList<>();
+        MyArrayList <Integer> arrListInt = new MyArrayList<>();
+        MyLinkedList <Integer> linkListInt = new MyLinkedList<>();
+        MyLinkedList <Integer> linkListStr = new MyLinkedList<>();
+
 
     }
 }
